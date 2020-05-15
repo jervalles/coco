620241afea8ec3b125adeb78d8f2c53367d4a1fa
Jeremy VALLES <jeremy.valles@niji.fr>
2020-05-15 20:50:23 +0200

    initialized server and installed dotenv and nodemon

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.end("Voilà la réponse du serveur")
+})
+
+server.listen(process.env.PORT || 3000)
