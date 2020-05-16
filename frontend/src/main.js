7a28843feab81fd0a0e748445cd788546cff1a21
Jeremy VALLES <jeremy.valles@niji.fr>
2020-05-16 16:41:40 +0200

    vue-router and first page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
+import router from '../router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  VueRouter,
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ frontend/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home-page">
+    <h1>Welcome Jérémy sur ton projet</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+
+</script>
+
+<style>
+
+.home-page {
+  background-color: red;
+}
+
+</style>
