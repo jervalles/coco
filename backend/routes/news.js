70bcc982d20784d0ea958111e262cef5a114f39d
Jeremy VALLES <jer.valles@outlook.com>
2020-08-26 21:13:03 +0200

    simple express app to add Admin role to a specified user

--- backend/routes/news.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const newsController = require('../controllers/news')
-
-router.get('/', newsController.getAllNews)
-
-module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,19 @@
-require('dotenv').config()
+const express = require('express')
+const app = express()
+var admin = require("firebase-admin");
 
-const http = require('http')
-const app = require('./app')
+var serviceAccount = require("./coco-248ba-firebase-adminsdk-fdiij-a81367e9c4.json");
 
-const normalizePort = val => {
-    const port = parseInt(val, 10);
-    if (isNaN(port)) {
-      return val;
-    }
-    if (port >= 0) {
-      return port;
-    }
-    return false;
-};
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-const errorHandler = error => {
-    if (error.syscall !== 'listen') {
-      throw error;
-    }
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-    switch (error.code) {
-      case 'EACCES':
-        console.error(bind + ' requires elevated privileges.');
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(bind + ' is already in use.');
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
-  };
-
-const server = http.createServer(app)
-
-server.on('error', errorHandler);
-server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://coco-248ba.firebaseio.com"
 });
 
-server.listen(port);
+app.get('/setAdmin', async (req,res) => {
+    admin.auth()
+        .setCustomUserClaims('xGmqAyUW1ZggN9nJtCPIXwWXfzy1', {
+            type: 'administrator'
+        }).then(() => console.log('done'))
+})
+
+app.listen(4000, () => console.log('listening on port 4000'))
