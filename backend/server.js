635b0adeaf1aa4fb942717ac25c0c64e068bea90
Jeremy VALLES <jer.valles@outlook.com>
2020-08-27 20:23:54 +0200

    user Login route

--- backend/server.js
+++ backend/server.js
@@ -21,23 +21,56 @@ app.use(
 
 // USER REGISTRATION || POST
 app.post("/signup", async (req, res, next) => {
-    const formData = req.body
-    
-    bcrypt.hash(formData.password, 10, (err, hash) => {
-        formData.password = hash
-        const newUser = formData
-          db.query("INSERT INTO user SET ?", [newUser], (err, results) => {
-            if (err) {
-              return res.status(400).send(err.sqlMessage)
-            }
-            newUser.password = undefined
-            newUser.id = results.insertId
-            return res.status(201).send({
-              user: newUser,
-              token: jwt.sign(JSON.stringify(newUser), jwtSecret)
-            })
+  const formData = req.body
+  
+  bcrypt.hash(formData.password, 10, (err, hash) => {
+    formData.password = hash
+    const newUser = formData
+      db.query("INSERT INTO user SET ?", [newUser], (err, results) => {
+        if (err) {
+          return res.status(400).send(err.sqlMessage)
+        }
+        newUser.password = undefined
+        newUser.id = results.insertId
+        return res.status(201).send({
+          user: newUser,
+          token: jwt.sign(JSON.stringify(newUser), jwtSecret) // ptete pas
+        })
+      })
+  })
+})
+
+// USER LOGIN || POST
+app.post("/login", async (req, res, next) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  db.query('SELECT * FROM user WHERE email = ?',[email], (err, results) => {
+    console.log({results})
+    if (err) {
+        res.json({
+          status:false,
+          message:'there are some error with query'
           })
-    })
+    } else {
+      if (results.length > 0) {
+        bcrypt.compare(password, results[0].password)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Wrong password' })
+          }
+          res.status(200).json({
+            userId: results[0].iduser,
+            token: jwt.sign({ userId: results[0].iduser}, jwtSecret)
+          })
+        })
+        .catch(err => res.status(500).json({ err }))
+        // return res.status(200).json(results)
+      } else {
+        return res.status(401).json({ error: 'User not found' })
+      }
+    }
+  })
 })
 
 
