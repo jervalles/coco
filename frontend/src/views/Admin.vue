2cda5073b78066059238d65b150ee8d1d548d1c3
Jeremy VALLES <jer.valles@outlook.com>
2020-09-02 23:54:56 +0200

    frontend - orders list

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="admin-page">
     ADMIN PAGE
+    <orders-list />
   </div>
 </template>
+
+<script>
+import OrdersList from '../components/Orders/OrdersList'
+
+export default {
+  name: 'Admin',
+  components: {
+    OrdersList
+  }
+}
+
+</script>
+
+<style>
+
+  .admin-page {
+    height: 100vh;
+  }
+
+</style>
