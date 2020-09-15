0cba2d55c399f0579d52b78307b9407e19aab520
Jeremy VALLES <jer.valles@outlook.com>
2020-09-15 09:28:19 +0200

    added items loading effect

--- frontend/src/components/Shopping/ItemsList.vue
+++ frontend/src/components/Shopping/ItemsList.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="items-list">
-    <v-progress-circular
-      v-if="loading"
-      id="loading"
-      :size="50"
-      color="primary"
-      indeterminate
-      ></v-progress-circular>
+    <div v-if="loading">
+      <vcl-facebook :width="300" :height="100" />
+      <vcl-facebook :width="300" :height="100" />
+      <vcl-facebook :width="300" :height="100" />
+      <vcl-facebook :width="300" :height="100" />
+    </div>
     <div v-for="(item, i) in items"
       :key="i"
     >
@@ -22,12 +21,14 @@
 </template>
 
 <script>
+import {VclFacebook} from 'vue-content-loading'
 import ItemsCard from './ItemsCard'
 
 export default {
   name: 'ItemsList',
   components: {
-    ItemsCard
+    ItemsCard,
+    VclFacebook
   },
   data() {
     return {
@@ -53,15 +54,17 @@ export default {
 
 <style lang="scss">
 
-.items-list {
-  background-color:rgb(240, 240, 240);
-  border-radius: 2px;
-  width: calc(100% - 114px);
-  display: flex;
-  flex-direction: column;
-  #loading {
-    align-self: center;
+  .items-list {
+    background-color:rgb(240, 240, 240);
+    border-radius: 2px;
+    width: calc(100% - 114px);
+    display: flex;
+    flex-direction: column;
+    height: 460px;
+    overflow: auto;
+    #loading {
+      align-self: center;
+    }
   }
-}
 
 </style>
